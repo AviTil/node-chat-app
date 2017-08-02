bc78165883710811d8678073cfbf686121540673
User <user@users-mbp.home>
2017-08-02 10:59:32 +0100

    add templating

--- public/js/index.js
+++ public/js/index.js
@@ -15,29 +15,29 @@
         socket.on('newMessage', function(message){
             
             var formattedTime = moment(message.createdAt).format('h:mm a')
+            var template = $("#message-template").html()
+            var html = Mustache.render(template, {
+                text: message.text,
+                from: message.from,
+                createdAt: formattedTime
+            });
             
-            var li = $("<li></li>")
-            li.text(message.from+" ("+formattedTime+"): "+message.text)
+            $("#messages").append(html)
             
-            $("#messages").append(li)
         })
 
 
         socket.on('newLocationMessage', function(message){
             
-            var li = $("<li></li>")
+           var formattedTime = moment(message.createdAt).format('h: mm a')
+           var template = $("#location-message-template").html() 
+            var html = Mustache.render(template, {
+                from: message.from,
+                url: message.url,
+                createdAt: formattedTime
+            });
             
-            var a = $('<a target="_blank"> My current location</a>')
-            
-            var formattedTime = moment(message.createdAt).format('h: mm a')
-            
-            
-            li.text(message.from+' ('+formattedTime+'): ')
-            a.attr('href', message.url)
-            
-            li.append(a)
-            
-            $("#messages").append(li)
+            $("#messages").append(html)
             
         })
 
